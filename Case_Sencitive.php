34bcd2f9f775cb7e5421cf00c3feeeec03df6cb0
NikitaBalasahebRaut <68865567+NikitaBalasahebRaut@users.noreply.github.com>
2021-02-10 19:22:05 +0530

    Add files via upload

---
+++ Case_Sencitive.php
@@ -0,0 +1,12 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "HELLO EVERYONE!<br>";
+EcHo "Hello Everyone!<br>";
+echo "hello everyone!<br>";
+?>
+
+</body>
+</html>
---
+++ Comment.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+echo "these program display all type comments";
+
+#shell type comments
+
+//c++ type comments
+
+/*
+c style commment
+*/
+
+?>
+</body>
+</html>
+
